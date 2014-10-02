4b2d493b1cb7ba2124b129e5fb376293dbfb0f3c
kevvvvv <kevin@fileperms.org>
2014-10-02 11:15:10 +0200

    remove targetlist, add ip-range scanning via iptools

--- ftpknocker.py
+++ ftpknocker.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
+import iptools
 import ftplib
 import threading
 
 argparser = ArgumentParser()
+argparser.add_argument('targets', nargs='+')
 argparser.add_argument('-t', '--threads',
                         action='store', default=20, type=int, dest='maxThreads', help='number of threads to use')
 argparser.add_argument('-w', '--wait',
                         action='store', default=2, type=int, dest='timeout', help='seconds to wait before timeout')
-argparser.add_argument('-f', '--file',
-                        action='store', dest='hostlist', required=True, help='file with target hosts')
+# TODO: add option for specifing a file with hosts
+# argparser.add_argument('-f', '--file',
+#                        action='store', dest='hostlist', help='optional file with target hosts')
 args = argparser.parse_args()
 
+targets = iptools.IpRangeList(*[host for host in args.targets])
 
 def tryFtpConnect(host):
 	host = host.strip()
@@ -23,12 +27,10 @@ def tryFtpConnect(host):
 		else:
 			print(host + ' DENIED')
 	except ftplib.all_errors:
-		print(host + ' ERROR')		
+		print(host + ' ERROR')
 
 
-with open(args.hostlist) as hosts:
-	for host in hosts:
-		while(threading.activeCount() >= args.maxThreads):
-			continue
-		threading.Thread(target=tryFtpConnect, args=(host,)).start()
-
+for target in targets:
+	while(threading.activeCount() >= args.maxThreads):
+		continue
+	threading.Thread(target=tryFtpConnect, args=(target,)).start()
