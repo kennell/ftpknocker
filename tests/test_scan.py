ce187e3cd029eca307eaf9e4900701cc13fcab1d
Kevin Kennell <kevin.kennell@gotthardt.com>
2017-11-20 18:43:25 +0100

    dummy scantests

--- tests/test_scan.py
+++ tests/test_scan.py
@@ -1,14 +1,30 @@
 import unittest
 
 
-KNOWN_ANONYMOUS_SERVERS = [
+ANONYMOUS_SERVERS = [
     '80.237.136.138',   # ftp.hosteurope.de
     '129.215.17.244',   # ftp.ed.ac.uk
     '134.109.228.1'     # ftp.tu-chemnitz.de
 ]
 
+NON_ANONYMOUS_SERVERS = [
+    '93.184.216.34',
+    '157.240.20.35'
+]
+
+MIXED_SERVERS = [
+    '80.237.136.138',   # ftp.hosteurope.de
+    '93.184.216.34'
+]
+
 
 class TestScan(unittest.TestCase):
 
-    def test_known_anonymous_servers(self):
-        assert(True)
+    def test_anonymous_servers(self):
+        self.assertEqual(True, True)
+
+    def test_non_anonymous_servers(self):
+        self.assertEqual(True, True)
+
+    def test_mixed_servers(self):
+        self.assertEqual(True, True)
