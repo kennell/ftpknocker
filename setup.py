8324c9b17f9b65887f814156c569a9ea3d70cf60
Kevin Kennell <kevin.kennell@gotthardt.com>
2017-11-20 14:50:20 +0100

    bump version to 1.0.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
   name = 'ftpknocker',
   packages = ['ftpknocker'],
-  version = '1.0.6',
+  version = '1.0.7',
   license = 'MIT',
   description = 'ftpknocker is a multi-threaded scanner for finding anonymous FTP servers',
   author = 'Kevin Kennell',
