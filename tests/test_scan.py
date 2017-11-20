41485136610c90a1e1a2ca57504394f6e01ed7cb
Kevin Kennell <kevin.kennell@gotthardt.com>
2017-11-20 20:09:13 +0100

    add scan tests

--- tests/test_scan.py
+++ tests/test_scan.py
@@ -1,30 +1,28 @@
 import unittest
+import subprocess
 
 
-ANONYMOUS_SERVERS = [
+ANONYMOUS_FTP_SERVERS = [
     '80.237.136.138',   # ftp.hosteurope.de
     '129.215.17.244',   # ftp.ed.ac.uk
     '134.109.228.1'     # ftp.tu-chemnitz.de
 ]
 
-NON_ANONYMOUS_SERVERS = [
+NO_FTP_SERVERS = [
     '93.184.216.34',
     '157.240.20.35'
-]
-
-MIXED_SERVERS = [
-    '80.237.136.138',   # ftp.hosteurope.de
-    '93.184.216.34'
 ]
 
 
 class TestScan(unittest.TestCase):
 
-    def test_anonymous_servers(self):
-        self.assertEqual(True, True)
+    def test_anonymous_ftp_servers(self):
+        output = subprocess.check_output(["ftpknocker"] + ANONYMOUS_FTP_SERVERS)
+        output = output.decode('utf-8')
+        for server in ANONYMOUS_FTP_SERVERS:
+            self.assertIn(server, output)
 
-    def test_non_anonymous_servers(self):
-        self.assertEqual(True, True)
-
-    def test_mixed_servers(self):
-        self.assertEqual(True, True)
+    def test_no_ftp_servers(self):
+        output = subprocess.check_output(["ftpknocker"] + NO_FTP_SERVERS)
+        output = output.decode('utf-8')
+        self.assertEqual("", output)
